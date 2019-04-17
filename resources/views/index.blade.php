f41c1277ac95a7b9e6fb4ac30319303123b42f43
WillyWax <williamtrans2011@gmail.com>
2019-04-17 07:47:46 +0300

    Changed the sample page to test permissions in the server

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,8 +11,10 @@
     <title>Hello, world!</title>
   </head>
   <body>
+  <div class="container">
+    
     <h1>Hello, world! Am Alive and Popping </h1>
-
+</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
